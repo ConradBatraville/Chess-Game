8101071ad6f2811ed8deb0dff0854dafb5db8de3
nbassell <nbassell@gmail.com>
2018-12-04 17:09:29 -0500

    Fix valid_pos

--- chess/board.rb
+++ chess/board.rb
@@ -7,8 +7,8 @@ class Board
 
   def initialize
     # debugger
-    @board = Array.new(8) { Array.new(8) { NullPiece.instance } }
-    p populate_board
+    @rows = Array.new(8) { Array.new(8) { NullPiece.instance } }
+    p populate_rows
     # private
     # @sentinel = NullPiece
 
@@ -32,63 +32,64 @@ class Board
 
     def [](pos)
       row, col = pos
-      board[row][col]
+      rows[row][col]
     end
     
     def []=(pos, piece)
       row, col = pos
-      board[row][col] = piece
+      rows[row][col] = piece
     end
 
     def valid_pos?(pos)
-      false
+      row, col = pos
+      row.between?(0,7) && col.between?(0, 7)
     end
   
 
-    def populate_board
+    def populate_rows
       
-      @board[0][0] = Piece.new("white", "Rook", [0, 0])
-      @board[0][7] = Piece.new("white", "Rook", [0, 7])
+      @rows[0][0] = Piece.new("white", "Rook", [0, 0])
+      @rows[0][7] = Piece.new("white", "Rook", [0, 7])
 
-      @board[0][1] = Piece.new("white", "Knight", [0, 1])
-      @board[0][6] = Piece.new("white", "Knight", [0, 6])
+      @rows[0][1] = Piece.new("white", "Knight", [0, 1])
+      @rows[0][6] = Piece.new("white", "Knight", [0, 6])
 
-      @board[0][2] = Piece.new("white", "Bishop", [0, 2])
-      @board[0][5] = Piece.new("white", "Bishop", [0, 5])
+      @rows[0][2] = Piece.new("white", "Bishop", [0, 2])
+      @rows[0][5] = Piece.new("white", "Bishop", [0, 5])
 
-      @board[0][3] = Piece.new("white", "Queen", [0, 3])
-      @board[0][4] = Piece.new("white", "King", [0, 4])
+      @rows[0][3] = Piece.new("white", "Queen", [0, 3])
+      @rows[0][4] = Piece.new("white", "King", [0, 4])
 
-      @board[1][0] = Piece.new("white", "Pawn", [1, 0])
-      @board[1][1] = Piece.new("white", "Pawn", [1, 1])
-      @board[1][2] = Piece.new("white", "Pawn", [1, 2])
-      @board[1][3] = Piece.new("white", "Pawn", [1, 3])
-      @board[1][4] = Piece.new("white", "Pawn", [1, 4])
-      @board[1][5] = Piece.new("white", "Pawn", [1, 5])
-      @board[1][6] = Piece.new("white", "Pawn", [1, 6])
-      @board[1][7] = Piece.new("white", "Pawn", [1, 7])
+      @rows[1][0] = Piece.new("white", "Pawn", [1, 0])
+      @rows[1][1] = Piece.new("white", "Pawn", [1, 1])
+      @rows[1][2] = Piece.new("white", "Pawn", [1, 2])
+      @rows[1][3] = Piece.new("white", "Pawn", [1, 3])
+      @rows[1][4] = Piece.new("white", "Pawn", [1, 4])
+      @rows[1][5] = Piece.new("white", "Pawn", [1, 5])
+      @rows[1][6] = Piece.new("white", "Pawn", [1, 6])
+      @rows[1][7] = Piece.new("white", "Pawn", [1, 7])
 
 
-      @board[7][0] = Piece.new("black", "Rook", [7, 0])
-      @board[7][7] = Piece.new("black", "Rook", [7, 7])
+      @rows[7][0] = Piece.new("black", "Rook", [7, 0])
+      @rows[7][7] = Piece.new("black", "Rook", [7, 7])
 
-      @board[7][1] = Piece.new("black", "Knight", [7, 1])
-      @board[7][6] = Piece.new("black", "Knight", [7, 6])
+      @rows[7][1] = Piece.new("black", "Knight", [7, 1])
+      @rows[7][6] = Piece.new("black", "Knight", [7, 6])
 
-      @board[7][2] = Piece.new("black", "Bishop", [7, 2])
-      @board[7][5] = Piece.new("black", "Bishop", [7, 5])
+      @rows[7][2] = Piece.new("black", "Bishop", [7, 2])
+      @rows[7][5] = Piece.new("black", "Bishop", [7, 5])
 
-      @board[7][3] = Piece.new("black", "Queen", [7, 3])
-      @board[7][4] = Piece.new("black", "King", [7, 4])
+      @rows[7][3] = Piece.new("black", "Queen", [7, 3])
+      @rows[7][4] = Piece.new("black", "King", [7, 4])
 
-      @board[6][0] = Piece.new("black", "Pawn", [6, 0])
-      @board[6][1] = Piece.new("black", "Pawn", [6, 1])
-      @board[6][2] = Piece.new("black", "Pawn", [6, 2])
-      @board[6][3] = Piece.new("black", "Pawn", [6, 3])
-      @board[6][4] = Piece.new("black", "Pawn", [6, 4])
-      @board[6][5] = Piece.new("black", "Pawn", [6, 5])
-      @board[6][6] = Piece.new("black", "Pawn", [6, 6])
-      @board[6][7] = Piece.new("black", "Pawn", [6, 7])
+      @rows[6][0] = Piece.new("black", "Pawn", [6, 0])
+      @rows[6][1] = Piece.new("black", "Pawn", [6, 1])
+      @rows[6][2] = Piece.new("black", "Pawn", [6, 2])
+      @rows[6][3] = Piece.new("black", "Pawn", [6, 3])
+      @rows[6][4] = Piece.new("black", "Pawn", [6, 4])
+      @rows[6][5] = Piece.new("black", "Pawn", [6, 5])
+      @rows[6][6] = Piece.new("black", "Pawn", [6, 6])
+      @rows[6][7] = Piece.new("black", "Pawn", [6, 7])
 
 
 
@@ -101,7 +102,7 @@ class Board
 
     private
 
-    attr_accessor :board
+    attr_accessor :rows
 
 
 end
