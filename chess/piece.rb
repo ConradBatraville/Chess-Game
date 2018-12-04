3bd02582ca8259b6ce256c2124286f0e97d098d5
nbassell <nbassell@gmail.com>
2018-12-04 12:08:37 -0500

    Add singleton to NullPiece class

--- chess/piece.rb
+++ chess/piece.rb
@@ -1,12 +1,22 @@
+require 'singleton'
+
 class Piece
+  attr_accessor :pos
+
   def initialize(color, name, pos)
     @color = color
     @name = name
-    @pos = []
+    @pos = pos
   end 
 end
 
 class NullPiece < Piece
+  include Singleton
+  
   def initialize
   end
+
+  
 end
+
+
